41d35c7b9249fa3bed9c892732b549043d3f13ba
DevilXD <bartosz.peszek@gmail.com>
2019-09-15 21:55:03 +0200

    Moved the doc sources to it's own directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -45,8 +45,11 @@ intersphinx_mapping = {
 
 default_role = "any"
 
+# Autodoc setting
+autodoc_typehints = "none"
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['../_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -66,4 +69,4 @@ html_theme_path = [corlab_theme.get_theme_dir()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
