bb60b4ca68a38632e87e63344d7a509936ae5644
DevilXD <bartosz.peszek@gmail.com>
2020-03-13 14:46:03 +0100

    Swapped the code places of CacheEntry and DataCache

--- arez/cache.py
+++ arez/cache.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 from datetime import datetime, timedelta
 from typing import Optional, Union, List, Dict, Iterator
@@ -7,161 +9,6 @@ from .utils import Lookup
 from .endpoint import Endpoint
 from .champion import Champion, Ability
 from .enumerations import Language, DeviceType
-
-
-class CacheEntry:
-    """
-    Represents a collection of champions, cards, talents and shop items.
-    You can get this one from the `PaladinsAPI.get_champion_info` or `DataCache.get_entry` methods.
-
-    Attributes
-    ----------
-    language : Language
-        The language of this entry.
-    champions : Lookup[Champion]
-        An object that lets you iterate over all champions.\n
-        Use ``list()`` to get a list instead.
-    abilities : Lookup[Ability]
-        An object that lets you iterate over all champion's abilities.\n
-        Use ``list()`` to get a list instead.
-    devices : Lookup[Device]
-        An object that lets you iterate over all devices (cards, talents and shop items).\n
-        This also includes other devices that are returned from the API,
-        but are considered invalid.\n
-        Use ``list()`` to get a list instead.
-    """
-    def __init__(
-        self, language: Language, expires_at: datetime, champions_data: dict, items_data: dict
-    ):
-        self.language = language
-        self._expires_at = expires_at
-        sorted_devices: Dict[int, List[Device]] = {}
-        for d in items_data:
-            champ_list = sorted_devices.setdefault(d["champion_id"], [])
-            champ_list.append(Device(d))
-        self.devices: Lookup[Device] = Lookup(d for dl in sorted_devices.values() for d in dl)
-        self.champions: Lookup[Champion] = Lookup(
-            Champion(sorted_devices.get(c["id"], []), c) for c in champions_data
-        )
-        self.abilities: Lookup[Ability] = Lookup(
-            a for c in self.champions for a in c.abilities
-        )
-
-    @property
-    def cards(self) -> Iterator[Device]:
-        """
-        A filtered iterator that lets you iterate over all cards.
-
-        Use ``list()`` to get a list instead.
-        """
-        dt = DeviceType["Card"]
-        return filter(lambda d: d.type == dt, self.devices)
-
-    @property
-    def talents(self) -> Iterator[Device]:
-        """
-        A filtered iterator that lets you iterate over all talents.
-
-        Use ``list()`` to get a list instead.
-        """
-        dt = DeviceType["Talent"]
-        return filter(lambda d: d.type == dt, self.devices)
-
-    @property
-    def items(self) -> Iterator[Device]:
-        """
-        A filtered iterator that lets you iterate over all shop items.
-
-        Use ``list()`` to get a list instead.
-        """
-        dt = DeviceType["Item"]
-        return filter(lambda d: d.type == dt, self.devices)
-
-    def get_champion(
-        self, champion: Union[str, int], *, fuzzy: bool = False
-    ) -> Optional[Champion]:
-        """
-        Returns a champion for the given Name or ID.
-        Case sensitive.
-
-        Parameters
-        ----------
-        champion : Union[str, int]
-            The Name or ID of the champion you want to get.
-        fuzzy : bool
-            When set to `True`, makes the Name search case insensitive.\n
-            Defaults to `False`.
-
-        Returns
-        -------
-        Optional[Champion]
-            The champion you requested.\n
-            `None` is returned if a champion with the requested Name or ID couldn't be found.
-        """
-        return self.champions.lookup(champion, fuzzy=fuzzy)
-
-    def get_card(self, card: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
-        """
-        Returns a champion's card for the given Name or ID.
-        Case sensitive.
-
-        Parameters
-        ----------
-        card : Union[str, int]
-            The Name or ID of the card you want to get.
-        fuzzy : bool
-            When set to `True`, makes the Name search case insensitive.\n
-            Defaults to `False`.
-
-        Returns
-        -------
-        Optional[Device]
-            The card you requested.\n
-            `None` is returned if a card with the requested Name or ID couldn't be found.
-        """
-        return self.devices.lookup(card, fuzzy=fuzzy)
-
-    def get_talent(self, talent: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
-        """
-        Returns a champion's talent for the given Name or ID.
-        Case sensitive.
-
-        Parameters
-        ----------
-        talent : Union[str, int]
-            The Name or ID of the talent you want to get.
-        fuzzy : bool
-            When set to `True`, makes the Name search case insensitive.\n
-            Defaults to `False`.
-
-        Returns
-        -------
-        Optional[Device]
-            The talent you requested.\n
-            `None` is returned if a talent with the requested Name or ID couldn't be found.
-        """
-        return self.devices.lookup(talent, fuzzy=fuzzy)
-
-    def get_item(self, item: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
-        """
-        Returns a shop item for the given Name or ID.
-        Case sensitive.
-
-        Parameters
-        ----------
-        item : Union[str, int]
-            The Name or ID of the shop item you want to get.
-        fuzzy : bool
-            When set to `True`, makes the Name search case insensitive.\n
-            Defaults to `False`.
-
-        Returns
-        -------
-        Optional[Device]
-            The shop item you requested.\n
-            `None` is returned if a shop item with the requested Name or ID couldn't be found.
-        """
-        return self.devices.lookup(item, fuzzy=fuzzy)
 
 
 class DataCache(Endpoint):
@@ -426,3 +273,158 @@ class DataCache(Endpoint):
         if entry:
             return entry.get_item(item, fuzzy=fuzzy)
         return None
+
+
+class CacheEntry:
+    """
+    Represents a collection of champions, cards, talents and shop items.
+    You can get this one from the `PaladinsAPI.get_champion_info` or `DataCache.get_entry` methods.
+
+    Attributes
+    ----------
+    language : Language
+        The language of this entry.
+    champions : Lookup[Champion]
+        An object that lets you iterate over all champions.\n
+        Use ``list()`` to get a list instead.
+    abilities : Lookup[Ability]
+        An object that lets you iterate over all champion's abilities.\n
+        Use ``list()`` to get a list instead.
+    devices : Lookup[Device]
+        An object that lets you iterate over all devices (cards, talents and shop items).\n
+        This also includes other devices that are returned from the API,
+        but are considered invalid.\n
+        Use ``list()`` to get a list instead.
+    """
+    def __init__(
+        self, language: Language, expires_at: datetime, champions_data: dict, items_data: dict
+    ):
+        self.language = language
+        self._expires_at = expires_at
+        sorted_devices: Dict[int, List[Device]] = {}
+        for d in items_data:
+            champ_list = sorted_devices.setdefault(d["champion_id"], [])
+            champ_list.append(Device(d))
+        self.devices: Lookup[Device] = Lookup(d for dl in sorted_devices.values() for d in dl)
+        self.champions: Lookup[Champion] = Lookup(
+            Champion(sorted_devices.get(c["id"], []), c) for c in champions_data
+        )
+        self.abilities: Lookup[Ability] = Lookup(
+            a for c in self.champions for a in c.abilities
+        )
+
+    @property
+    def cards(self) -> Iterator[Device]:
+        """
+        A filtered iterator that lets you iterate over all cards.
+
+        Use ``list()`` to get a list instead.
+        """
+        dt = DeviceType["Card"]
+        return filter(lambda d: d.type == dt, self.devices)
+
+    @property
+    def talents(self) -> Iterator[Device]:
+        """
+        A filtered iterator that lets you iterate over all talents.
+
+        Use ``list()`` to get a list instead.
+        """
+        dt = DeviceType["Talent"]
+        return filter(lambda d: d.type == dt, self.devices)
+
+    @property
+    def items(self) -> Iterator[Device]:
+        """
+        A filtered iterator that lets you iterate over all shop items.
+
+        Use ``list()`` to get a list instead.
+        """
+        dt = DeviceType["Item"]
+        return filter(lambda d: d.type == dt, self.devices)
+
+    def get_champion(
+        self, champion: Union[str, int], *, fuzzy: bool = False
+    ) -> Optional[Champion]:
+        """
+        Returns a champion for the given Name or ID.
+        Case sensitive.
+
+        Parameters
+        ----------
+        champion : Union[str, int]
+            The Name or ID of the champion you want to get.
+        fuzzy : bool
+            When set to `True`, makes the Name search case insensitive.\n
+            Defaults to `False`.
+
+        Returns
+        -------
+        Optional[Champion]
+            The champion you requested.\n
+            `None` is returned if a champion with the requested Name or ID couldn't be found.
+        """
+        return self.champions.lookup(champion, fuzzy=fuzzy)
+
+    def get_card(self, card: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
+        """
+        Returns a champion's card for the given Name or ID.
+        Case sensitive.
+
+        Parameters
+        ----------
+        card : Union[str, int]
+            The Name or ID of the card you want to get.
+        fuzzy : bool
+            When set to `True`, makes the Name search case insensitive.\n
+            Defaults to `False`.
+
+        Returns
+        -------
+        Optional[Device]
+            The card you requested.\n
+            `None` is returned if a card with the requested Name or ID couldn't be found.
+        """
+        return self.devices.lookup(card, fuzzy=fuzzy)
+
+    def get_talent(self, talent: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
+        """
+        Returns a champion's talent for the given Name or ID.
+        Case sensitive.
+
+        Parameters
+        ----------
+        talent : Union[str, int]
+            The Name or ID of the talent you want to get.
+        fuzzy : bool
+            When set to `True`, makes the Name search case insensitive.\n
+            Defaults to `False`.
+
+        Returns
+        -------
+        Optional[Device]
+            The talent you requested.\n
+            `None` is returned if a talent with the requested Name or ID couldn't be found.
+        """
+        return self.devices.lookup(talent, fuzzy=fuzzy)
+
+    def get_item(self, item: Union[str, int], *, fuzzy: bool = False) -> Optional[Device]:
+        """
+        Returns a shop item for the given Name or ID.
+        Case sensitive.
+
+        Parameters
+        ----------
+        item : Union[str, int]
+            The Name or ID of the shop item you want to get.
+        fuzzy : bool
+            When set to `True`, makes the Name search case insensitive.\n
+            Defaults to `False`.
+
+        Returns
+        -------
+        Optional[Device]
+            The shop item you requested.\n
+            `None` is returned if a shop item with the requested Name or ID couldn't be found.
+        """
+        return self.devices.lookup(item, fuzzy=fuzzy)
