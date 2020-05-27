85db4cfa4c196ea57b777eee22579b1d12a1289c
DevilXD <bartosz.peszek@gmail.com>
2020-05-27 15:44:03 +0200

    Added 404 and 503 tests

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,10 +1,39 @@
+import arez
 import pytest
+
+
+# test ping
+@pytest.mark.vcr()
+@pytest.mark.base()
+@pytest.mark.asyncio()
+@pytest.mark.dependency()
+async def test_ping(api):
+    await api.request("ping")
 
 
 # test session creation
 @pytest.mark.vcr()
 @pytest.mark.base()
 @pytest.mark.asyncio()
+@pytest.mark.dependency("test_ping")
 @pytest.mark.dependency(scope="session")
 async def test_session(api):
     await api.request("testsession")
+
+
+# test error handling
+@pytest.mark.vcr()
+@pytest.mark.base()
+@pytest.mark.asyncio()
+async def test_404(api):
+    with pytest.raises(arez.HTTPException):
+        await api.request("nonexistent_endpoint")
+
+
+# test unavailable
+@pytest.mark.vcr()
+@pytest.mark.base()
+@pytest.mark.asyncio()
+async def test_503(api):
+    with pytest.raises(arez.Unavailable):
+        await api.request("unavailable")
